d987134a312237fc335029889d37e33468d13e26
Diego Andre Poli <diegoandrepoli@gmail.com>
2019-03-15 10:03:25 -0300

    Create json stringfy utils

---
+++ saler/src/main/java/com/saler/saler/deal/utils/JsonStrinfy.java
@@ -0,0 +1,24 @@
+package com.saler.saler.deal.utils;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+/**
+ * Stringfy json as object
+ * @author Diego Andre Poli <diegoandrepoli@gmail.com>
+ */
+public class JsonStrinfy {
+
+	/**
+	 * Object as json
+	 * @param object
+	 * @return json
+	 */
+	public static String asJsonString(final Object obj) {
+		try {
+			return new ObjectMapper().writeValueAsString(obj);
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+}
