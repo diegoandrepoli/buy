921710b42270a3f5fa2e38dd78b52c87a2f6ce96
Diego Andre Poli <diegoandrepoli@gmail.com>
2019-03-15 09:09:30 -0300

    Add saler application

---
+++ saler/src/main/java/com/saler/saler/SalerApplication.java
@@ -0,0 +1,21 @@
+package com.saler.saler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Saler application runner
+ * @author Diego Andre Poli <diegoandrepoli@gmail.com>
+ */
+@SpringBootApplication
+public class SalerApplication {
+
+	/**
+	 * Main application
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		SpringApplication.run(SalerApplication.class, args);
+	}
+
+}
