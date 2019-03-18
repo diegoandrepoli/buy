e29048dc47af5c1d93d489d537416c361c65d847
Diego Andre Poli <diegoandrepoli@gmail.com>
2019-03-17 22:07:33 -0300

    Add sample data to start application

--- saler/src/main/java/com/saler/saler/SalerApplication.java
+++ saler/src/main/java/com/saler/saler/SalerApplication.java
@@ -1,21 +1,38 @@
 package com.saler.saler;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.saler.saler.data.sample.SampleData;
+import com.saler.saler.deal.repository.DealRepository;
+
 /**
  * Saler application runner
+ * 
  * @author Diego Andre Poli <diegoandrepoli@gmail.com>
  */
 @SpringBootApplication
 public class SalerApplication {
 
 	/**
+	 * Auto wired - start sample data on intial application
+	 * @param deal repository
+	 */
+	@Autowired
+	public SalerApplication(DealRepository dealRepository) {
+		SampleData.generate(dealRepository);
+		
+	}
+
+	/**
 	 * Main application
+	 * 
 	 * @param args
 	 */
 	public static void main(String[] args) {
 		SpringApplication.run(SalerApplication.class, args);
+
 	}
 
 }
